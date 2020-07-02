2914155c1163bfe9f35b846907742e060dfc6d77
Sven Liebig <liebigsv@gmail.com>
2020-07-02 22:31:43 +0200

    stand

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { makeExecutableSchema } = require('graphql-tools')
+
+
+let links = [{
+    id: "l1",
+    url: "http://google.com",
+    description: "full yolo jacket"
+}]
+
+// 1
+const typeDefs = `
+    type Query {
+        info: String!
+        feed: [Link!]!
+    }
+
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
+    }
+`
+
+// 2
+const resolvers = {
+    Query: {
+        info: () => "null",
+        feed: () => links
+    },
+    Link: {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url
+    }
+}
+
+const schema = makeExecutableSchema({ resolvers, typeDefs })
+
+// 3
+const server = new GraphQLServer({
+    schema
+})
+
+server.start(() => {
+    console.log(`Server is running on http://localhost:4000`)
+})
+
